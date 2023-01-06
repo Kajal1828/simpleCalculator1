9d4c2b1f96c8b7f7fd01eb6a4fd19e69bd9f825a
kajal <kajalneha.nk@gmail.com>
2023-01-07 00:01:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import {add, sub, mult, div} from './Calc';
+import React from 'react'
+import { useState } from 'react';
 
+const App = () => {
+    const state = useState();
+    const[count, setCount]=useState(0); 
+   
 
-function App(){
-    return(
-        <>
-        <ul>
-            <li>{add(40, 4)}</li>
-            <li>{sub(40, 4)}</li>
-            <li>{mult(40,4)}</li>
-            <li>{div(40, 3)}</li>
-        </ul>
-        
-        </>
-    );
+const IncNum = () =>{
+    setCount(count + 1);
+
 }
+
+
+  return (
+    <>
+    <h1>{count}</h1>
+    <button onClick={IncNum}>Click Me</button>
+    
+    </>
+  );
+};
+
 export default App
